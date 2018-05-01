a87a94dc1524e3450f775f8026cd3d5cf6f5ffc8
christofferEkblom <cekmw09@student.lnu.se>
2018-05-01 22:54:43 +0200

    ⬇️ create download button

--- src/view/ToolPanelView.dart
+++ src/view/ToolPanelView.dart
@@ -5,20 +5,24 @@ class ToolPanelView implements View {
   DivElement _toolPanel;
   ButtonElement _plusButton;
   ButtonElement _minusButton;
+  ButtonElement _downloadButton;
 
   ButtonElement get plusButton => _plusButton;
   ButtonElement get minusButton => _minusButton;
+  ButtonElement get downloadButton => _downloadButton;
 
   ToolPanelView() {
     _toolPanel = new DivElement();
     _toolPanel.id = 'tool-panel';
     _plusButton = new ButtonElement();
     _minusButton = new ButtonElement();
+    _downloadButton = new ButtonElement();
   }
 
   void generate() {
     _toolPanel.append(_generatePlusButton());
     _toolPanel.append(_generateMinusButton());
+    _toolPanel.append(_generateDownloadButton());
     document.body.children.add(_toolPanel);
   }
 
@@ -32,5 +36,11 @@ class ToolPanelView implements View {
     _minusButton.id = 'remove';
     _minusButton.appendText('-');
     return _minusButton;
+  }
+
+  ButtonElement _generateDownloadButton() {
+    _downloadButton.id = 'download';
+    _downloadButton.appendText('↓');
+    return _downloadButton;
   }
 }
