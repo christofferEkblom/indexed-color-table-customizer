ea65036170c1969b1060f0098c03fd2996945e27
christofferEkblom <cekmw09@student.lnu.se>
2018-05-03 19:49:40 +0200

    ✨ Sample table generator

---
+++ Sample table generator/Program.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import glob, binascii, json
+
+hexColorLength = 6
+fileExtension = '*.act'
+outputFile = 'Sample.json'
+
+data = {}
+
+for file in glob.glob(fileExtension):
+  with open(file, 'rb') as f:
+    hexdata = binascii.hexlify(f.read())
+    data[file] = [hexdata[i: i + hexColorLength] for i in range(0, len(hexdata), hexColorLength)]
+
+jsonFile = open(outputFile, 'w') 
+jsonFile.write(json.dumps(data) + '\n')
